1dfaae75a3ae012ff3816125615fb88253828c15
Manuel Luitz <manuel.luitz@gmail.com>
2017-05-13 11:13:49 +0200

    Added GPLv3 header to all files

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ all:
 	pyuic4 gui/settings.ui > settings_window.py
 	pyuic4 gui/about.ui > about_window.py
 
+clean:
+	rm *.pyc 2>/dev/null
+
--- settings.py
+++ settings.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+# This file is part of Camino.
+# Copyright (C) 2017 Manuel Luitz <manuel.luitz@gmail.com>
+#
+# Camino is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Camino is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Camino. If not, see <http://www.gnu.org/licenses/>.
+#
 from PyQt4 import QtGui,QtCore
 import settings_window
 
